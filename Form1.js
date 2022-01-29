52078ca33e7956fba2a704ccd0b9c37c636b78f4
Gyanwee28 <59122478+Gyanwee28@users.noreply.github.com>
2022-01-29 18:53:10 +0530

    Add files via upload

---
+++ Form1.js
@@ -0,0 +1,52 @@
+import Card from '../ui/Card';
+import React from "react";
+// import { Editor } from "@tinymce/tinymce-react";
+import {useRef} from 'react';
+import classes from './Form.module.css'
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
+function Form(){
+    const titleInputRef = useRef();
+    const descriptionRef = useRef();
+    function submitHandler(event){
+       
+        event.preventDefault();
+        
+        const enteredTitle = titleInputRef.current.value;
+        const enteredDescription=descriptionRef.current.value;
+        const noteData={
+            title: enteredTitle,
+            description:enteredDescription,
+        };
+        
+        console.log(noteData);
+    }
+    
+    return (<Card>
+        
+        <form className={classes.form} onSubmit={submitHandler}>
+            <div className ={classes.control}>
+                <label htmlFor='title'>Title</label>
+                <input type="text" required id="title" ref={titleInputRef}/>
+            </div>
+            <div className ={classes.control}>
+                <label htmlFor='description'>Description</label>
+                <Editor id='description' ref={descriptionRef} />
+                {/* <textarea id='description' ref={descriptionRef}></textarea> */}
+                {/* <Editor
+                id='description' ref={descriptionRef}
+                init={{
+                    height: 500,
+                    menubar: false
+                     }}
+                     /> */}
+            </div>
+            <div className={classes.actions}>
+                <button>Save</button>
+            </div>
+        </form>
+    </Card>);
+};
+
+export default Form;
